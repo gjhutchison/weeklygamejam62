2ae09f6ee6fa02cc5546ff55fa348ba03abff41c
ecrows <evan.of.crows@gmail.com>
2018-09-15 17:16:13 -0400

    Adding wolf following script, fixes to sheep colliders.

--- Assets/Scripts/EatSheep.cs
+++ Assets/Scripts/EatSheep.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 
 public class EatSheep : MonoBehaviour {
 
+	private bool isCollidingSheep = false;
 	private string corpseSpriteNames = "Sprites/sheepcorpses";
 	private Sprite[] sprites;
 
@@ -16,13 +17,18 @@ public class EatSheep : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		isCollidingSheep = false;
 	}
 		
 	void OnTriggerEnter2D(Collider2D other)
 	{
-		Debug.Log(other.gameObject.tag);
 		if (other.gameObject.tag == "Sheep") {
+			if (isCollidingSheep) {
+				return;
+			}
+
+			isCollidingSheep = true;
+				
 			// Eat the sheep (switch spite, and other handling)
 			other.enabled = false;
 			other.gameObject.GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Length)];
