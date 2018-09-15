c3dc1ffe48b57188e1b83b4d16570ba52133c782
ecrows <evan.of.crows@gmail.com>
2018-09-15 19:52:52 -0400

    Rudimentary player movement.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public SpriteRenderer sr;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+		transform.Translate(x, 0, 0);
+		transform.Translate(0, z, 0);
+
+		if (x >= 0) {
+			sr.flipX = false;
+		} else {
+			sr.flipX = true;
+		}
+	}
+}
