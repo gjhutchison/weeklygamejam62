5e56d89887041036f4e766cc58ca603aa7627dd9
gjhutchison <grahamjhutchison@gmail.com>
2018-09-15 15:37:15 -0600

    Wolf Prefab and redone Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,23 +5,71 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 	public SpriteRenderer sr;
 
+    public Rigidbody2D _rb;
+
+    private bool _up, _down, _left, _right;
+
+    private bool _flipped;
+
+    private bool _controller;
+
+    private readonly static float FORCE = 750.0f;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        _up = false;
+        _down = false;
+        _left = false;
+        _right = false;
+        _controller = false;
+
+    }
 	
+    void inputCheck() {
+        if (Input.GetKeyDown(KeyCode.A)) {
+            _left = true;
+        } else if (Input.GetKeyDown(KeyCode.D)) {
+            _right = true;
+        } else if (Input.GetKeyDown(KeyCode.W)) {
+            _up = true;
+        } else if (Input.GetKeyDown(KeyCode.S)) {
+            _down = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.A)) {
+            _left = false;
+        } else if (Input.GetKeyUp(KeyCode.D)) {
+            _right = false;
+        } else if (Input.GetKeyUp(KeyCode.W)) {
+            _up = false;
+        } else if (Input.GetKeyUp(KeyCode.S)) {
+            _down = false;
+        }
+    }
+
 	// Update is called once per frame
 	void Update () {
-		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
-		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        Vector2 force = new Vector2();
+        inputCheck();
 
-		transform.Translate(x, 0, 0);
-		transform.Translate(0, z, 0);
 
-		if (x >= 0) {
-			sr.flipX = false;
-		} else {
-			sr.flipX = true;
-		}
-	}
+        if (_left) {
+            force.x = -FORCE * Time.deltaTime;
+        }
+
+        if(_right) {
+            force.x = FORCE * Time.deltaTime;
+        }
+
+        if(_up) {
+            force.y = FORCE * Time.deltaTime;
+        }
+
+        if(_down) {
+            force.y = -FORCE * Time.deltaTime;
+        }
+
+        _rb.AddForce(force);
+        sr.flipX = _rb.velocity.x < 0;
+    }
 }
