e89d79816623616bace0bf8b01b39e2c3b6adfb0
gjhutchison <grahamjhutchison@gmail.com>
2018-09-16 15:19:35 -0600

    Shepard Chasing when player enters vision cone

---
+++ Assets/Scripts/Shepard Scripts/ShepardController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShepardController : MonoBehaviour {
+
+    public GameObject _visionCone;
+    public Rigidbody2D _hitbox;
+    public GameObject _player;
+    public SpriteBobber _spriteBobber;
+
+    private ShepardState _state;
+
+    private Vector2 _target;
+
+    private readonly static float FORCE = 675;
+
+    private enum ShepardState {
+        IDLE,
+        SEARCH,
+        CHASE,
+        INVESTIGATE
+    };
+
+	// Use this for initialization
+	void Start () {
+        _state = ShepardState.IDLE;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        switch (_state) {
+            case ShepardState.CHASE:
+                doChase();
+                break;
+        }
+	}
+
+    private void doChase() {
+        Vector2 playerPos = new Vector2(_player.transform.position.x, _player.transform.position.y);
+        Vector2 currentPos = new Vector2(transform.position.x, transform.position.y);
+        
+        float angle = Mathf.Atan2(playerPos.y - currentPos.y, playerPos.x - currentPos.x);
+        float totalForce = FORCE * Time.deltaTime;
+        Vector2 force = new Vector2(Mathf.Cos(angle) * totalForce, Mathf.Sin(angle) * totalForce);
+
+        _hitbox.AddForce(force);
+        _spriteBobber.activate();
+    }
+
+    public void setChasing() {
+        _state = ShepardState.CHASE;
+    }
+}
---
+++ Assets/Scripts/Shepard Scripts/ShepardVisionCone.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShepardVisionCone : MonoBehaviour {
+
+    public ShepardController _shepardController;
+
+    private float _currentAngle;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag == "Player") {
+            spotPlayer(other.gameObject);
+        }
+    }
+
+    private void spotPlayer(GameObject player) {
+        _shepardController.setChasing();
+        print("I see you!");
+        
+    }
+
+    public void setLookTarget(Vector2 target) {
+
+    }
+}
