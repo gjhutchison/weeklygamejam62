0b8deb5289523b45b288e5cc507ba5547ad81cef
gjhutchison <grahamjhutchison@gmail.com>
2018-09-18 18:19:45 -0600

    Removing garbage code

--- Assets/Scripts/BushColliderScript.cs
+++ Assets/Scripts/BushColliderScript.cs
@@ -5,35 +5,26 @@ using UnityEngine;
 public class BushColliderScript : MonoBehaviour {
 
     public PlayerController _playerController;
-    private bool _playerInBush;
 
 	// Use this for initialization
 	void Start () {
-        _playerInBush = false;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (_playerInBush) {
-            float distance = Vector2.Distance(_playerController.gameObject.transform.position, transform.parent.gameObject.transform.position);
-            if(distance > 1) {
-                _playerInBush = false;
-                _playerController.setSlowed(false);
-            }
-        }
 	}
 
     void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.tag == "Bush") {
             _playerController.setSlowed(true);
-            _playerInBush = true;
+            //_playerInBush = true;
         }
     }
 
     void OnTriggerExit2D(Collider2D other) {
         if (other.gameObject.tag == "Bush") {
             _playerController.setSlowed(false);
-            _playerInBush = false;
+            //_playerInBush = false;
         }
     }
 }
